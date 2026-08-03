cfb7b6c9fc41e315fd6f21cee31d528b804df083
andr-235 <143680929+andr-235@users.noreply.github.com>
2026-08-04 09:02:52 +1000

    refactor(vk): avoid eager command consumer import

--- services/vk-service/app/tasks/__init__.py
+++ services/vk-service/app/tasks/__init__.py
@@ -1,10 +1,8 @@
 from .kafka_consumer import TaskEventsConsumer
 from .outbox_worker import OutboxPublisher, publish_outbox_forever
-from .vk_commands_consumer import VkExecutionCommandsConsumer
 
 __all__ = [
     "OutboxPublisher",
     "publish_outbox_forever",
     "TaskEventsConsumer",
-    "VkExecutionCommandsConsumer",
 ]
