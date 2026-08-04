2d9fba470376650c330664aea22363a190ef16ee
andr-235 <143680929+andr-235@users.noreply.github.com>
2026-08-04 16:40:57 +1000

    fix(p2): remove legacy task event service exports

--- services/vk-service/app/services/__init__.py
+++ services/vk-service/app/services/__init__.py
@@ -1,7 +1,6 @@
 from .domain_events_service import OutboxService
 from .ingestion_service import IngestionService
 from .ok_friends.exporter import OkFriendsExportService
-from .task_events_service import TaskEvent, TaskEventsService
 from .vk_friends.exporter import VkFriendsExportService
 from .vk_groups_service import VkGroupsService
 
@@ -9,10 +8,6 @@ __all__ = [
     "VkFriendsExportService",
     "OkFriendsExportService",
     "IngestionService",
-    "TaskEventsService",
-    "TaskEvent",
     "OutboxService",
     "VkGroupsService",
 ]
-
-
